0baafc05ea0a316c70ff61e8676f6eb13e9a7f30
aleerangel <familiagamer1054@gmail.com>
2026-08-31 13:58:54 -0300

    armazenar solucao PRCP

--- exercicios/PRCP/prcp.h
+++ exercicios/PRCP/prcp.h
@@ -1,6 +1,11 @@
 #define MAX_PON 1000
 #define MAX_POS 4
 
+typedef struct tSolucao {
+    int fo;
+    int vet_sol[MAX_PON + 1];
+} Solucao;
+
 int num_pon;
 int num_pos;
 int vet_num_con_pos[MAX_PON * MAX_POS + 1];
